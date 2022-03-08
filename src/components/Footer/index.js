7401e537e7a2e06cf961dc7bccd1951a843578b3
Auzan <auzan.ip@gmail.com>
2022-03-08 11:54:50 +0700

    update login page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,33 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import './style.css'
+import "./style.css";
 
-
-function Index( props ) {
-    return (
-        <footer className="col-12 footer">
-            <div className="justify-content-center">
-                <div className="col-12 text-light">
-                    <ul className="list-inline d-flex footer-list">
-                        <li>
-                            <a target="_blank" rel="noreferrer" href="https://s.id/geschool" className="footer-link bold">Road Map to Geschool</a>
-                        </li>
-                    </ul>
-                    <ul className="list-inline d-flex footer-list link-list">
-                        <li>
-                            <a target="_blank" rel="noreferrer" href="https://www.pdkjateng.go.id/" className="footer-link">Dinas Pendidikan Provinsi Jawa Tengah</a>
-                        </li>
-                        <li>
-                            <a target="_blank" rel="noreferrer" href="https://jateng.kemenag.go.id/" className="footer-link">Kemenag Provinsi Jawa Tengah</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="col-12 copyright mt-2">
-                    &copy; {props.copyright}
-                </div>
-            </div>
-        </footer>
-    );
+function Index(props) {
+  return (
+    <footer className="col-12 footer">
+      <div className="justify-content-center">
+        <div className="col-12 text-light">
+          <ul className="list-inline d-flex footer-list link-list">
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.pdkjateng.go.id/"
+                className="footer-link"
+              >
+                Dinas Pendidikan Provinsi Jawa Tengah
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://jateng.kemenag.go.id/"
+                className="footer-link"
+              >
+                Kemenag Provinsi Jawa Tengah
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-12 copyright mt-2">
+          &copy; {props.copyright}{" "}
+          <a target="_blank" rel="noreferrer" href="https://s.id/geschool">
+            Road Map to Geschool
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Index;
